dd028b2f0b08d056272ffc3cf78a22631656c644
Lukas Jungmann <lukas.jungmann@oracle.com>
2019-11-28 12:45:09 +0100

    add copyright check to travis (#1373)
    * fix copyrights
    * add copyright check to travis
    
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jaxb-ri/bundles/ri/src/main/resources/bin/xjc.bat
+++ jaxb-ri/bundles/ri/src/main/resources/bin/xjc.bat
@@ -4,16 +4,10 @@ REM
 REM  Copyright (c) 1997, 2019 Oracle and/or its affiliates. All rights reserved.
 REM
 REM  This program and the accompanying materials are made available under the
-REM  terms of the Eclipse Public License v. 2.0, which is available at
-REM  http://www.eclipse.org/legal/epl-2.0.
+REM  terms of the Eclipse Distribution License v. 1.0, which is available at
+REM  http://www.eclipse.org/org/documents/edl-v10.php.
 REM
-REM  This Source Code may also be made available under the following Secondary
-REM  Licenses when the conditions for such availability set forth in the
-REM  Eclipse Public License v. 2.0 are satisfied: GNU General Public License,
-REM  version 2 with the GNU Classpath Exception, which is available at
-REM  https://www.gnu.org/software/classpath/license.html.
-REM
-REM  SPDX-License-Identifier: EPL-2.0 OR GPL-2.0 WITH Classpath-exception-2.0
+REM  SPDX-License-Identifier: BSD-3-Clause
 REM
 
 rem
