ec0e86712e85e2713bdcfc9119591b816e63453f
Michael Davis <michael.davis@essvote.com>
2020-02-19 15:31:58 -0600

    xjc.sh: Fix setting JAXB_HOME with relative links
    If xjc.sh is a relative link it should be changing directory based on the location of the link.
    Not the directory the command is run from.
    
    Signed-off-by: Michael Davis <michael.davis@essvote.com>

--- jaxb-ri/bundles/ri/src/main/resources/bin/xjc.sh
+++ jaxb-ri/bundles/ri/src/main/resources/bin/xjc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 1997, 2019 Oracle and/or its affiliates. All rights reserved.
+# Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
 #
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -36,7 +36,6 @@ then
     JAXB_HOME=`dirname "$PRG"`/..
     
     # make it fully qualified
-    cd "$saveddir"
     JAXB_HOME=`cd "$JAXB_HOME" && pwd`
     
     cd "$saveddir"
