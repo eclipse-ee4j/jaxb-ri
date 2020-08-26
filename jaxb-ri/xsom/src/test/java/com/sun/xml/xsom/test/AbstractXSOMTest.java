ebafe3d4678c3987a9c61240cb65619b6906484c
Lukas Jungmann <lukas.jungmann@oracle.com>
2020-08-26 21:45:12 +0200

    fix tests
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jaxb-ri/xsom/src/test/java/com/sun/xml/xsom/test/AbstractXSOMTest.java
+++ jaxb-ri/xsom/src/test/java/com/sun/xml/xsom/test/AbstractXSOMTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -7,6 +7,8 @@
  *
  * SPDX-License-Identifier: BSD-3-Clause
  */
+
+package com.sun.xml.xsom.test;
 
 import junit.framework.TestCase;
 import com.sun.xml.xsom.XSSchemaSet;
