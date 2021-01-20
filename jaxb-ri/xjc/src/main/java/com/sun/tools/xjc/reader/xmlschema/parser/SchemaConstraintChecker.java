eeed555c1d14c748ead7a26e9e5d087c085a3aac
Lukas Jungmann <lukas.jungmann@oracle.com>
2021-01-20 16:08:31 +0100

    #1502: XJC: fails to process XSD files without systemId
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jaxb-ri/xjc/src/main/java/com/sun/tools/xjc/reader/xmlschema/parser/SchemaConstraintChecker.java
+++ jaxb-ri/xjc/src/main/java/com/sun/tools/xjc/reader/xmlschema/parser/SchemaConstraintChecker.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -62,7 +62,7 @@ public class SchemaConstraintChecker {
                     try {
                         // XSOM passes the namespace URI to the publicID parameter.
                         // we do the same here .
-                        InputSource is = entityResolver.resolveEntity(namespaceURI, systemId);
+                        InputSource is = entityResolver.resolveEntity(namespaceURI, systemId == null ? "" : systemId);
                         if(is==null)    return null;
                         return new LSInputSAXWrapper(is);
                     } catch (SAXException e) {
