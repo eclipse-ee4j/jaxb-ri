502ee95835521661b12535e0adc67a45e437c4d5
Lukas Jungmann <lukas.jungmann@oracle.com>
2021-01-20 16:08:31 +0100

    #1502: XJC: fails to process XSD files without systemId
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jaxb-ri/xsom/src/main/java/com/sun/xml/xsom/impl/parser/NGCCRuntimeEx.java
+++ jaxb-ri/xsom/src/main/java/com/sun/xml/xsom/impl/parser/NGCCRuntimeEx.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -175,7 +175,7 @@ public class NGCCRuntimeEx extends NGCCRuntime implements PatcherManager {
                 baseUri=documentSystemId;
 
             EntityResolver er = parser.getEntityResolver();
-            String systemId = null;
+            String systemId = "";
 
             if (relativeUri!=null) {
                 if (isAbsolute(relativeUri)) {
