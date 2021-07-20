baedaec6300232385213e269aca786ff2c9a6fcc
Lukas Jungmann <lukas.jungmann@oracle.com>
2021-07-20 12:43:32 +0200

    cp
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jaxb-ri/xsom/src/test/java/com/sun/xml/xsom/test/AbstractXSOMTest.java
+++ jaxb-ri/xsom/src/test/java/com/sun/xml/xsom/test/AbstractXSOMTest.java
@@ -1,7 +1,7 @@
 package com.sun.xml.xsom.test;
 
 /*
- * Copyright (c) 1997, 2020 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2021 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
