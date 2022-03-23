1e9bd15b5d74e5a2e5e46872f44f88c4dd68acdf
Lukas Jungmann <lukas.jungmann@oracle.com>
2022-03-23 16:36:04 +0100

    missing exports in osgi bundle
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jaxb-ri/bundles/osgi/osgi/src/main/java/module-info.java
+++ jaxb-ri/bundles/osgi/osgi/src/main/java/module-info.java
@@ -50,6 +50,9 @@ module com.sun.xml.bind.osgi {
     exports org.glassfish.jaxb.runtime.v2.model.impl;
     exports org.glassfish.jaxb.runtime.v2.model.runtime;
     exports org.glassfish.jaxb.runtime.v2.runtime;
+    exports org.glassfish.jaxb.runtime.v2.runtime.reflect;
+    exports org.glassfish.jaxb.runtime.v2.runtime.output;
+    exports org.glassfish.jaxb.runtime.v2.runtime.property;
     exports org.glassfish.jaxb.runtime.v2.runtime.unmarshaller;
     exports org.glassfish.jaxb.runtime.v2.schemagen;
     exports org.glassfish.jaxb.runtime.v2.schemagen.xmlschema;
