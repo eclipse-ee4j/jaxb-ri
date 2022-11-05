28097dd4e7ecc874719ecaa674e1ce08987a345a
ExE Boss <ExE-Boss@code.ExE-Boss.tech>
2022-11-05 14:10:00 +0100

    [codemodel] Use canonical modifier order when generating `JMods`
    Signed-off-by: ExE Boss <ExE-Boss@code.ExE-Boss.tech>

--- jaxb-ri/codemodel/codemodel/src/main/java/com/sun/codemodel/JMod.java
+++ jaxb-ri/codemodel/codemodel/src/main/java/com/sun/codemodel/JMod.java
@@ -19,15 +19,15 @@ public final class JMod {
     private JMod() {
     }
 
-    public final static int NONE         = 0x000;
-    public final static int PUBLIC       = 0x001;
-    public final static int PROTECTED    = 0x002;
-    public final static int PRIVATE      = 0x004;
-    public final static int FINAL        = 0x008;
-    public final static int STATIC       = 0x010;
-    public final static int ABSTRACT     = 0x020;
-    public final static int NATIVE       = 0x040;
-    public final static int SYNCHRONIZED = 0x080;
-    public final static int TRANSIENT    = 0x100;
-    public final static int VOLATILE     = 0x200;
+    public static final int NONE         = 0x000;
+    public static final int PUBLIC       = 0x001;
+    public static final int PROTECTED    = 0x002;
+    public static final int PRIVATE      = 0x004;
+    public static final int FINAL        = 0x008;
+    public static final int STATIC       = 0x010;
+    public static final int ABSTRACT     = 0x020;
+    public static final int NATIVE       = 0x040;
+    public static final int SYNCHRONIZED = 0x080;
+    public static final int TRANSIENT    = 0x100;
+    public static final int VOLATILE     = 0x200;
 }
