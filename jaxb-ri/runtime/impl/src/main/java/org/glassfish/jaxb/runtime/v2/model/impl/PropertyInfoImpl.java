2269a1d80914221a1be73026b7c2e94bb0a4f11f
Lukas Jungmann <lukas.jungmann@oracle.com>
2023-10-06 19:04:46 +0200

    Fix regressions introduced by #1715
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- jaxb-ri/runtime/impl/src/main/java/org/glassfish/jaxb/runtime/v2/model/impl/PropertyInfoImpl.java
+++ jaxb-ri/runtime/impl/src/main/java/org/glassfish/jaxb/runtime/v2/model/impl/PropertyInfoImpl.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1997, 2023 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1997, 2022 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -350,7 +350,7 @@ abstract class PropertyInfoImpl<T,C,F,M>
                     uri = "";
                 }
             } else {
-                uri = parent.builder.defaultNsUri;
+                uri = "";
             }
         }
         return new QName(uri.intern(),local.intern());
