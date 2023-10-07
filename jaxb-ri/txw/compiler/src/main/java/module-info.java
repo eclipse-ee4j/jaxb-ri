0a5ccd40a576bef3befa79f8a0593bec66c8c437
Lukas Jungmann <lukas.jungmann@oracle.com>
2023-10-07 15:28:23 +0200

    #1757: txw runtime and compiler should have different module names
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>
    (cherry picked from commit 8bcb69a4aca4af411b964133976ea2540daa30c9)

--- jaxb-ri/txw/compiler/src/main/java/module-info.java
+++ jaxb-ri/txw/compiler/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2021, 2023 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -8,7 +8,8 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
-module com.sun.tools.txw2 {
+@SuppressWarnings({"module"})
+module com.sun.tools.txwc2 {
     requires transitive java.xml;
 
     requires com.sun.xml.txw2;
