b5f86ef88d8e48ca84568aa9c5859ad5cf5719ec
Gerlach, Winfried <Winfried.Gerlach@draeger.com>
2025-11-12 06:53:25 +0100

    remove redundant method calls in DatatypeConverterImpl

--- jaxb-ri/runtime/impl/src/main/java/org/glassfish/jaxb/runtime/DatatypeConverterImpl.java
+++ jaxb-ri/runtime/impl/src/main/java/org/glassfish/jaxb/runtime/DatatypeConverterImpl.java
@@ -54,7 +54,7 @@ public final class DatatypeConverterImpl implements DatatypeConverterInterface {
     }
 
     public static BigInteger _parseInteger(CharSequence s) {
-        return new BigInteger(removeOptionalPlus(WhiteSpaceProcessor.trim(s)).toString());
+        return new BigInteger(WhiteSpaceProcessor.trim(s).toString());
     }
 
     public static String _printInteger(BigInteger val) {
@@ -96,7 +96,7 @@ public final class DatatypeConverterImpl implements DatatypeConverterInterface {
     }
 
     public static long _parseLong(CharSequence s) {
-        return Long.parseLong(removeOptionalPlus(WhiteSpaceProcessor.trim(s)).toString());
+        return Long.parseLong(WhiteSpaceProcessor.trim(s).toString());
     }
 
     public static short _parseShort(CharSequence s) {
@@ -678,25 +678,6 @@ public final class DatatypeConverterImpl implements DatatypeConverterInterface {
         }
 
         return ptr;
-    }
-
-    private static CharSequence removeOptionalPlus(CharSequence s) {
-        int len = s.length();
-
-        if (len <= 1 || s.charAt(0) != '+') {
-            return s;
-        }
-
-        s = s.subSequence(1, len);
-        char ch = s.charAt(0);
-        if ('0' <= ch && ch <= '9') {
-            return s;
-        }
-        if ('.' == ch) {
-            return s;
-        }
-
-        throw new NumberFormatException();
     }
 
     private static boolean isDigitOrPeriodOrSign(char ch) {
